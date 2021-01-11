ead67ab5668d2340dc9333488595caf250ebce40
Joao Farias <joao@thatsabug.com>
2021-01-11 22:06:33 +0100

    Persistance Configuration object

---
+++ src/main/kotlin/com/thatsabug/conferencedemo/config/PersistanceConfiguration.kt
@@ -0,0 +1,17 @@
+package com.thatsabug.conferencedemo.config
+
+import org.springframework.boot.jdbc.DataSourceBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import javax.sql.DataSource
+
+@Configuration
+class PersistenceConfiguration {
+    @Bean
+    fun dataSource() : DataSource =
+        DataSourceBuilder.create()
+            .url("jdbc:postgresql://localhost:5432/conference_app")
+            .username("postgres")
+            .password("Welcome")
+            .build()
+}
